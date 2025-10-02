7ca40ff09c9fc5747a3c2584ce2d83208f4b225f
jeonggyun <wjdrbs77777@gmail.com>
2025-10-02 13:23:32 +0900

    https://github.com/yahahahoho/Java-Calculator/issues/2

---
+++ Calculatorr/src/AdvancedCalculator.java
@@ -0,0 +1,15 @@
+public class AdvancedCalculator extends Calculator {
+    @Override
+    public int multiply()
+    {
+        result = num1 * num2;
+        return result;
+    }
+    @Override
+    public int divide()
+    {
+        result = num1 / num2;
+        return result;
+    }
+
+}
--- Calculatorr/src/Main.java
+++ Calculatorr/src/Main.java
@@ -4,5 +4,12 @@ public class Main {
         calc_add.setNumbers(10, 2);
         calc_add.add();
         calc_add.displayResult("+");
+
+
+
+        AdvancedCalculator calc_mul = new AdvancedCalculator();
+        calc_mul.setNumbers(10, 2);
+        calc_mul.multiply();
+        calc_mul.displayResult("*");
     }
 }
