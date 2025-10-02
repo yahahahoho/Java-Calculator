05efc1e17f316ccc90872331f9279540eafffd00
jeonggyun <wjdrbs77777@gmail.com>
2025-10-02 13:38:43 +0900

    AdvancedCalculator주석 추가 완료

--- Calculatorr/src/AdvancedCalculator.java
+++ Calculatorr/src/AdvancedCalculator.java
@@ -1,10 +1,16 @@
+// 계산기 v2.0 관련 클래스
+//https://github.com/yahahahoho/Java-Calculator/issues/1
 public class AdvancedCalculator extends Calculator {
+    //계산기 v2.0 곱하기 기능 개발 완료
+    // https://github.com/yahahahoho/Java-Calculator/issues/2
     @Override
     public int multiply()
     {
         result = num1 * num2;
         return result;
     }
+    //나누기 기능 개발완료
+    //https://github.com/yahahahoho/Java-Calculator/issues/3
     @Override
     public int divide()
     {
