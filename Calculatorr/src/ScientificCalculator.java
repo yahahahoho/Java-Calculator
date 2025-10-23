746028eed7ce4ffa13507fff433e6072b19f19aa
jeonggyun <wjdrbs77777@gmail.com>
2025-10-23 14:03:44 +0900

    ScientificCalculator 주석 추가완료

--- Calculatorr/src/ScientificCalculator.java
+++ Calculatorr/src/ScientificCalculator.java
@@ -1,8 +1,12 @@
 public class ScientificCalculator extends AdvancedCalculator {
+    //https://github.com/yahahahoho/Java-Calculator/issues/9
+    //계산기 v 3.0 제곱연산 개발 완료
 
     public void square() {
         this.result = this.num1 * this.num1;
     }
 }
 
+//계산기 v 3.0 관련 클래스
+// https://github.com/yahahahoho/Java-Calculator/issues/8
 
